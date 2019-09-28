c40d6249cf46e9fd170a03fcf6ba0b9770b313b8
taoliu12 <taoliudesign@gmail.com>
2019-09-27 23:54:16 -0700

    fix user has_many

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
     has_many :votes
     has_many :users, through: :votes
-    belongs_to: :user 
+    belongs_to :user 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
-    
+
     has_many :votes
     has_many :authored_posts, class_name: "Post", foreign_key: 'author_id'
-    has_many :voted_posts, through: :votes, class_name: "Post"
+    has_many :voted_posts, through: :votes, source: :post
 end
