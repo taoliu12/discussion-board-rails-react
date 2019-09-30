db5dd4bd9251b03580820aa1b8b3cdb3baccc3e0
taoliu12 <taoliudesign@gmail.com>
2019-09-30 12:08:15 -0700

    prevent unauthorized post edits

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :redirect_unauthorized_user, only: [:edit, :update, :destroy]
 
   def index
   end
@@ -40,6 +41,13 @@ class PostsController < ApplicationController
   def set_post
     @post = Post.find(params[:id])
   end
+
+  def redirect_unauthorized_user
+    if @post.user != current_user
+      redirect_to posts_path
+    end
+  end
+  
 
   def post_params
     params.require(:post).permit(:title, :body)
