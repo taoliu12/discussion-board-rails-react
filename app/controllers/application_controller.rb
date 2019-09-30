07e0334e1ae11a45ad134765c9af0f77d093237d
taoliu12 <taoliudesign@gmail.com>
2019-09-30 12:26:20 -0700

    posts destroy action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
     private
 
     def current_user
-        User.first
+        User.last
         #User.find_by(id: session[:user_id]) #find_by returns nil if no user found
     end
     
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,6 +34,8 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
