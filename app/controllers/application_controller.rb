306bb267a75c865ec1584506a90006b6b1568b0b
taoliu12 <taoliudesign@gmail.com>
2019-09-30 10:22:01 -0700

    posts create action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+    
+    private
+
+    def current_user
+        User.first
+        #User.find_by(id: session[:user_id]) #find_by returns nil if no user found
+    end
+    
+    def logged_in?
+        !!current_user
+    end
+    
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,16 +6,28 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  def create
-    raise params.inspect
+  def create     
+    @post = current_user.authored_posts.build(post_params)        
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end  
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
