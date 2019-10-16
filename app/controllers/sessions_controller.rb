11cfc4d29081fe972e460ffa046f5642014b1921
taoliu12 <taoliudesign@gmail.com>
2019-10-16 13:38:01 -0700

    finish omniauth login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,8 +13,31 @@ class SessionsController < ApplicationController
     end     
   end
 
+  def create_from_omniauth
+    user = User.find_or_create_by(email: omniauth[:info][:email]) do |user|
+      user.username = omniauth[:info][:email] #need to remove email format later
+      user.email = omniauth[:info][:email]
+      user.password = SecureRandom.hex
+    end
+
+    session[:user_id] = user.id
+
+    if logged_in? 
+      flash[:message] = 'Login via Google successful.'
+    else
+      flash[:message] = 'Something went wrong. Please try again.'
+    end
+    redirect_to root_path
+  end
+
   def destroy
     session.clear     
     redirect_to root_path
+  end
+
+  private
+
+  def omniauth
+    request.env['omniauth.auth']
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_28_044707) do
+ActiveRecord::Schema.define(version: 2019_10_16_194658) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_09_28_044707) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
   create_table "votes", force: :cascade do |t|
