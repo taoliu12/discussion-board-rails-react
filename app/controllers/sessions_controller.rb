5909298b2919931cb3abb231de0236e188af73d0
taoliu12 <taoliudesign@gmail.com>
2021-01-30 08:01:32 -0800

    style forms

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   def new
+    redirect_to posts_path if current_user
   end
 
   def create
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     def new
+        redirect_to posts_path if current_user
         @user = User.new
     end
   
