f7e2549cedff0805166249bd4890f6fa0136432f
tao <taoliudesign@gmail.com>
2023-02-17 22:07:43 -0800

    wip

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,10 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RailsReddit
+module Test1
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
