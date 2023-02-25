76c47fd632b64b2d6679eba1094d2682b879741e
tao <taoliudesign@gmail.com>
2023-02-25 10:40:19 -0800

    backend

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+    def create 
+        @post = Post.find_by_id(params[:post_id])
+        if !@post
+            message = 'Post Does Not Exist'
+            render json: { error: message }, status: :unprocessable_entity
+        end
+        
+        @vote = current_user.votes.find_or_initialize_by(post: @post)
+                 
+        if @vote.value == vote_params[:value].to_i
+            message = 'Already Voted'
+            @vote.destroy
+        elsif @vote.update(vote_params)      
+            message = 'Vote Successful'
+        else
+            message = `Something went wrong`
+        end
+        render json: PostSerializer.new(@post)
+    end   
+end
