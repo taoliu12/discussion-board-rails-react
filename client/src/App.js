3fbfeb1e9c18d88b38ade91ba6a6503e2502acc5
Tao Liu <werewaswas@live.com>
2023-03-08 18:31:01 -0800

    scrolltotop and styling top margin

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,8 @@ import ResponsiveAppBar from './ResponsiveAppBar';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import {Link, Typography} from '@mui/material';
+import ScrollToTop from './components/ScrollToTop';
+import Toolbar from '@mui/material/Toolbar';
 
 function App() {
   const [ loggedInUser , setLoggedInUser ] = useState( null )
@@ -44,38 +46,39 @@ function App() {
   return (
     <div className="App">
       <ResponsiveAppBar loggedInUser={loggedInUser} handleLogout={handleLogout}/>
-      <header className="App-header"></header>
-      <div className="posts-container">
-      <main>         
+      <Toolbar id="back-to-top-anchor" sx={{mb: '31px'}}/>
       
-      <Routes>
-        <Route path='/signup' element={<SignupForm setLoggedInUser={setLoggedInUser}/>}/>                          
-        <Route path='/login' element={<LoginForm setLoggedInUser={setLoggedInUser}/>}/>   
-        <Route path='/' element={<Posts loggedInUser={loggedInUser} />} />   
-        <Route path='/posts' element={<Posts loggedInUser={loggedInUser} />} />   
-        <Route path='/posts/:postId' element={<PostShow loggedInUser={loggedInUser}/>}/>   
-        <Route path='/posts/:postId/edit' element={<EditPost posts={posts} loggedInUser={loggedInUser}/>}/>   
-        <Route path='/posts/new' element={<NewPost posts={posts} setPost={setPosts}/>}/>   
-        <Route path='/myposts' element={<MyPosts loggedInUser={loggedInUser}/>}/>   
-        <Route path='/profile' element={<Profile loggedInUser={loggedInUser}/>}/>   
-        <Route path='/t' element={<Test/>}/>   
-      </Routes>
-      </main>
-      <br></br>
-      <footer style={{alignItems: 'center'}}>
-               
-        <Typography sx={{ display: { fontSize: '11px', color: 'gray'}, m: 1 }} >         
-          Made by Tao Liu 
-        </Typography >
+      <div className="posts-container">
+        <main>         
         
-      <Link href="https://github.com/taoliu12/discussion-board-rails-react" target="_blank" rel="noopener">
-        <GitHubIcon sx={{ display: { fontSize: '23px', color: 'gray'}, mr: 1 }} />
-      </Link>
-      <Link href="https://www.linkedin.com/in/tao-liu-228105176/" target="_blank" rel="noopener">
-        <LinkedInIcon sx={{ display: { fontSize: '23px', color: 'gray'}, mr: 1 }} />
-      </Link>
-      </footer>
+        <Routes>
+          <Route path='/signup' element={<SignupForm setLoggedInUser={setLoggedInUser}/>}/>                          
+          <Route path='/login' element={<LoginForm setLoggedInUser={setLoggedInUser}/>}/>   
+          <Route path='/' element={<Posts loggedInUser={loggedInUser} />} />   
+          <Route path='/posts' element={<Posts loggedInUser={loggedInUser} />} />   
+          <Route path='/posts/:postId' element={<PostShow loggedInUser={loggedInUser}/>}/>   
+          <Route path='/posts/:postId/edit' element={<EditPost posts={posts} loggedInUser={loggedInUser}/>}/>   
+          <Route path='/posts/new' element={<NewPost posts={posts} setPost={setPosts}/>}/>   
+          <Route path='/myposts' element={<MyPosts loggedInUser={loggedInUser}/>}/>   
+          <Route path='/profile' element={<Profile loggedInUser={loggedInUser}/>}/>   
+          <Route path='/t' element={<Test/>}/>   
+        </Routes>
+        </main>
+        <br></br>
+        <footer style={{alignItems: 'center'}}> 
+          <Typography sx={{ display: { fontSize: '11px', color: 'gray'}, m: 1 }} >         
+            Made by Tao Liu 
+          </Typography >
+          
+        <Link href="https://github.com/taoliu12/discussion-board-rails-react" target="_blank" rel="noopener">
+          <GitHubIcon sx={{ display: { fontSize: '23px', color: 'gray'}, mr: 1 }} />
+        </Link>
+        <Link href="https://www.linkedin.com/in/tao-liu-228105176/" target="_blank" rel="noopener">
+          <LinkedInIcon sx={{ display: { fontSize: '23px', color: 'gray'}, mr: 1 }} />
+        </Link>
+        </footer>
       </div> 
+      <ScrollToTop sx={{ mr: 1 }} />
     </div>
   );
 }
