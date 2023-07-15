53b7d2cfe0ddb54a84bab979b7eaae87024560dd
Tao Liu <taoliudesign@gmail.com>
2023-07-14 22:33:43 -0700

    Update ResponsiveAppBar.js

--- client/src/ResponsiveAppBar.js
+++ client/src/ResponsiveAppBar.js
@@ -1,33 +1,34 @@
-import * as React from 'react';
-import { NavLink, Link } from 'react-router-dom';
-import DarkModeButton from './DarkModeButton';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem'; 
-import ForumIcon from '@mui/icons-material/Forum';
-import { textShadow } from '@mui/system';
-import NavbarButton from './NavbarButton';
+import * as React from "react";
+import { NavLink, Link } from "react-router-dom";
+import DarkModeButton from "./DarkModeButton";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import ForumIcon from "@mui/icons-material/Forum";
+import { textShadow } from "@mui/system";
+import NavbarButton from "./NavbarButton";
 
 const pages = [
-  { title: 'All Posts', route: '/posts' },
-  { title: 'My Posts', route: '/myposts' },
-  { title: 'Create Post', route: '/posts/new' }
+  { title: "All Posts", route: "/posts" },
+  { title: "My Posts", route: "/myposts" },
+  { title: "Create Post", route: "/posts/new" },
 ];
 
 const settings = [
-  { title: 'Profile',  route: '/profile' }, 
-  { title: 'Logout', route: '/logout' }];
+  { title: "Profile", route: "/profile" },
+  { title: "Logout", route: "/logout" },
+];
 
-function ResponsiveAppBar({loggedInUser, handleLogout}) {
+function ResponsiveAppBar({ loggedInUser, handleLogout }) {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
 
@@ -47,38 +48,61 @@ function ResponsiveAppBar({loggedInUser, handleLogout}) {
   };
 
   return (
-    <AppBar position="static" sx={{
-      color: 'white',
-      position: `fixed !important`,
-      backgroundColor: 'light blue',
-      textShadow: '0px 0px 3px #00000',
-      zIndex: '11',
-    }}>
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Link to='/posts'> 
-            <ForumIcon sx={{ display: { color: 'white',
-            fontSize: '33px',xs: 'none', sm: 'flex' }, mr: 1 }} />
+    <AppBar
+      position="static"
+      sx={{
+        color: "white",
+        position: `fixed !important`,
+        backgroundColor: "light blue",
+        textShadow: "0px 0px 3px #00000",
+        zIndex: "11",
+        height: "70px",
+      }}
+    >
+      <Container
+        maxWidth="xl"
+      >
+        <Toolbar
+          disableGutters
+          sx={{
+            alignItems: "top",
+          }}
+        >
+          <Link to="/posts">
+            <ForumIcon
+              sx={{
+                display: {
+                  color: "white",
+                  fontSize: "33px",
+                  xs: "none",
+                  sm: "flex",
+                },
+                mr: 1,
+              }}
+            />
           </Link>
-          <Typography 
+          <Typography
             noWrap
             component="a"
             href="/"
             sx={{
               mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'Fredoka One',
+              display: { xs: "none", md: "flex" },
+              fontFamily: "Fredoka One",
               fontWeight: 500,
-              fontSize: '25px',
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
+              fontSize: "25px",
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
             }}
           >
             Talky
           </Typography>
           {/*hamburger*/}
-          <Box id="test" sx={{ flexGrow: 1, display: { xs: 'flex', sm: 'none' } }}>
+          <Box
+            id="test"
+            sx={{ flexGrow: 1, display: { xs: "flex", sm: "none" } }}
+          >
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -88,113 +112,127 @@ function ResponsiveAppBar({loggedInUser, handleLogout}) {
               color="inherit"
             >
               <MenuIcon />
-            </IconButton> 
-            <Menu 
+            </IconButton>
+            <Menu
               id="menu-appbar"
               anchorEl={anchorElNav}
               anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
+                vertical: "bottom",
+                horizontal: "left",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
+                vertical: "top",
+                horizontal: "left",
               }}
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
               sx={{
-                display: { xs: 'block', md: 'none' },
+                display: { xs: "block", md: "none" },
               }}
             >
-                <Link to='/posts'>
-                  <MenuItem onClick={handleCloseNavMenu}>
-                    <Typography textAlign="center">All Posts</Typography>
-                  </MenuItem>
-                </Link>
-                { loggedInUser ? 
-                [
-                  <Link to='/myposts'>
-                    <MenuItem onClick={handleCloseNavMenu}>
-                      <Typography textAlign="center">My Posts</Typography>
-                    </MenuItem>
-                  </Link> ,          
-                  <Link to='/posts/new'>
-                    <MenuItem onClick={handleCloseNavMenu}>
-                      <Typography textAlign="center">Create Post</Typography>
-                    </MenuItem>
-                  </Link> 
-                ] : [
-                  <Link to='/signup'>
-                    <MenuItem onClick={handleCloseNavMenu}>
-                      <Typography textAlign="center">Sign Up</Typography>
-                    </MenuItem>
-                  </Link> ,          
-                  <Link to='/login'>
-                    <MenuItem onClick={handleCloseNavMenu}>
-                      <Typography textAlign="center">Login</Typography>
-                    </MenuItem>
-                  </Link>           
-                                
-                ] }
+              <Link to="/posts">
+                <MenuItem onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">All Posts</Typography>
+                </MenuItem>
+              </Link>
+              {loggedInUser
+                ? [
+                    <Link to="/myposts">
+                      <MenuItem onClick={handleCloseNavMenu}>
+                        <Typography textAlign="center">My Posts</Typography>
+                      </MenuItem>
+                    </Link>,
+                    <Link to="/posts/new">
+                      <MenuItem onClick={handleCloseNavMenu}>
+                        <Typography textAlign="center">Create Post</Typography>
+                      </MenuItem>
+                    </Link>,
+                  ]
+                : [
+                    <Link to="/signup">
+                      <MenuItem onClick={handleCloseNavMenu}>
+                        <Typography textAlign="center">Sign Up</Typography>
+                      </MenuItem>
+                    </Link>,
+                    <Link to="/login">
+                      <MenuItem onClick={handleCloseNavMenu}>
+                        <Typography textAlign="center">Login</Typography>
+                      </MenuItem>
+                    </Link>,
+                  ]}
             </Menu>
           </Box>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', sm: 'flex', md: 'flex' } }}> 
-              <NavbarButton item='All Posts' route='/posts'/>      
-              { loggedInUser ? 
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", sm: "flex", md: "flex" },
+              alignItems: "top",
+            }}
+          >
+            <NavbarButton item="All Posts" route="/posts" />
+            {loggedInUser ? (
               <>
-                <NavbarButton item='My Posts' route='/myposts'/>
-                <NavbarButton item='Create Post' route='/posts/new'/>               
-              </> : <>
-                <NavbarButton item='Login' route='/login'/>               
-                <NavbarButton item='Sign Up' route='/signup'/>               
-              </> }
-              {/* <NavbarButton item='About' route='/about'/>       */}
+                <NavbarButton item="My Posts" route="/myposts" />
+                <NavbarButton item="Create Post" route="/posts/new" />
+              </>
+            ) : (
+              <>
+                <NavbarButton item="Login" route="/login" />
+                <NavbarButton item="Sign Up" route="/signup" />
+              </>
+            )}
+            {/* <NavbarButton item='About' route='/about'/>       */}
           </Box>
           <DarkModeButton />
 
-          { loggedInUser &&
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-     
-                <Link to='/profile' >
-                <MenuItem onClick={handleCloseUserMenu}>
-                  <Typography sx={{ my: 2, color: 'black'}} textAlign="center">
-                     Profile
-                  </Typography>
-                </MenuItem>
+          {loggedInUser && (
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="Remy Sharp" />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                <Link to="/profile">
+                  <MenuItem onClick={handleCloseUserMenu}>
+                    <Typography
+                      sx={{ my: 2, color: "black" }}
+                      textAlign="center"
+                    >
+                      Profile
+                    </Typography>
+                  </MenuItem>
                 </Link>
-                <Link to='' onClick={handleLogout} >
-                <MenuItem onClick={handleCloseUserMenu}>
-                  <Typography sx={{ my: 2, color: 'black'}} textAlign="center">
-                     Logout
-                  </Typography>
-                </MenuItem>
+                <Link to="" onClick={handleLogout}>
+                  <MenuItem onClick={handleCloseUserMenu}>
+                    <Typography
+                      sx={{ my: 2, color: "black" }}
+                      textAlign="center"
+                    >
+                      Logout
+                    </Typography>
+                  </MenuItem>
                 </Link>
-            </Menu>
-          </Box> 
-        }
+              </Menu>
+            </Box>
+          )}
         </Toolbar>
       </Container>
     </AppBar>
