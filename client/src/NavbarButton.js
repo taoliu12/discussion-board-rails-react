c77f2f48d92bfe582408d8b4e382de149251ddd8
Tao Liu <taoliudesign@gmail.com>
2023-07-14 23:37:16 -0700

    underline hover navbar links

--- client/src/NavbarButton.js
+++ client/src/NavbarButton.js
@@ -1,16 +1,30 @@
-import * as React from 'react';
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import { styled } from '@mui/material/styles';
+import Link from "@mui/material/Link";
+import Button from "@mui/material/Button";
 
-import Button from '@mui/material/Button';
+const StyledLink = styled(Link)`
+  color: white;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
 
-export default function NavbarButton({item, route}) {
-return (
-    <Link className='top-nav-bar-link' style={{color: 'white'}} to={route}>
-        <Button                 
-        sx={{mx: .7, my: 2, fontSize: '1.2rem', color: 'white', display: 'block', boxShadow: '0px 0px 3px #00000', }}
-        >
-            {item}
-        </Button>
-    </Link>
-    )
+export default function NavbarButton({ item, route }) {
+  return (
+    <StyledLink className="top-nav-bar-link" to={route}>
+      <Button
+        sx={{
+          mx: 0.7,
+          my: 2,
+          fontSize: "1.2rem",
+          color: "white",
+          display: "block",
+          boxShadow: "0px 0px 3px #00000",
+        }}
+      >
+        {item}
+      </Button>
+    </StyledLink>
+  );
 }
