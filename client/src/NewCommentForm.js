684b883467240f54add5a970f375d13c13c2c223
Tao Liu <taoliudesign@gmail.com>
2023-07-15 23:23:44 -0700

    display child comment form

--- client/src/NewCommentForm.js
+++ client/src/NewCommentForm.js
@@ -1,48 +1,60 @@
-import React, { useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { useForm } from "react-hook-form";
 
-export default function NewCommentForm({setComments}) {
-    const { register, handleSubmit, watch, reset, formState: { errors } } = useForm();     
-    const [customError, setCustomError] = useState('')
-    const navigate = useNavigate();
-    let { postId } = useParams();
-      
-    const handleFormSubmit = ( data ) => {  
-      fetch( `/posts/${postId}/comments`, 
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify( data )
-        } 
-      ) 
-      .then( r => r.json() )
-      .then( json => {       
+export default function NewCommentForm({ setComments }) {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm();
+  const [customError, setCustomError] = useState("");
+  const navigate = useNavigate();
+  let { postId } = useParams();
+
+  const handleFormSubmit = (data) => {
+    fetch(`/posts/${postId}/comments`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    })
+      .then((r) => r.json())
+      .then((json) => {
         if (!json.error) {
-          let comments = json.included                  
-          console.log('new comment submitted', json)           
-          setComments(comments)
-          reset({content: ''})  
+          let comments = json.included;
+          console.log("new comment submitted", json);
+          setComments(comments);
+          reset({ content: "" });
         } else {
-            setCustomError("Something went wrong. Please try again.")
-            reset({content: ''})     
+          setCustomError("Something went wrong. Please try again.");
+          reset({ content: "" });
         }
-      }) 
-    }
- 
+      });
+  };
+
   return (
-  <>     
-    <div className="comment-form-container"> 
-      <form onSubmit={handleSubmit(handleFormSubmit)}>
-          <p>{errors.content && <span className='formError'>(This field is required)</span>}</p> 
-          <textarea                  
+    <>
+      <div className="comment-form-container">
+        <form onSubmit={handleSubmit(handleFormSubmit)}>
+          <p>
+            {errors.content && (
+              <span className="formError">(This field is required)</span>
+            )}
+          </p>
+          <textarea
             placeholder="Create a Comment"
-            name="content" 
+            name="content"
             {...register("content", { required: true })}
           />
-          <div> <input type="submit"  className="small-submit-btn"/></div>
+          <div>
+            {" "}
+            <input type="submit" className="small-submit-btn" />
+          </div>
           <p> {customError}</p>
-      </form>
-    </div>
-  </> 
-)};       
+        </form>
+      </div>
+    </>
+  );
+}
