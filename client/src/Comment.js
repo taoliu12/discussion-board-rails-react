9434d481490f67194cf9adfa98095c2b37b6d183
Tao Liu <taoliudesign@gmail.com>
2023-07-16 13:48:15 -0700

    wip

--- client/src/Comment.js
+++ client/src/Comment.js
@@ -2,14 +2,19 @@ import React, { useState, useEffect } from "react";
 import Card from "@mui/material/Card";
 import NewCommentForm from "./NewCommentForm";
 import { Box, Button } from "@mui/material";
+import { IconButton } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 
 export default function Comment({ comment, setComments }) {
   const [toggleReplyForm, setToggleReplyForm] = useState(false);
 
-  const handleToggleReply = () => {
+  const displayReplyForm = () => {
     setToggleReplyForm(true);
   };
-  debugger;
+  const hideReplyForm = () => {
+    setToggleReplyForm(false);
+  };
+
   return (
     <>
       <Card
@@ -29,27 +34,31 @@ export default function Comment({ comment, setComments }) {
           {comment.attributes.author_name} -{" "}
           {comment.attributes.formatted_created_at}
         </p>
-        <Button onClick={handleToggleReply}>Reply</Button>
+        <Button onClick={displayReplyForm}>Reply</Button>
       </Card>
       {toggleReplyForm && (
-        <NewCommentForm
-          parentCommentId={comment.id}
-          setComments={setComments}
-        />
+        <Box display="flex" alignItems="top" minWidth="500px">
+          <NewCommentForm
+            parentCommentId={comment.id}
+            setComments={setComments}
+          />
+          <IconButton onClick={hideReplyForm} sx={{ width: 50, height: 50 }}>
+            <CloseIcon />
+          </IconButton>
+        </Box>
       )}
 
-{comment.attributes.child_comments && (
-  <Box sx={{ pl: 2 }}>
-    {comment.attributes.child_comments.map((child_comment) => (
-      <Comment
-        comment={child_comment.data}
-        setComments={setComments}
-        key={child_comment.id}
-      />
-    ))}
-  </Box>
-)}
-
+      {comment.attributes.child_comments && (
+        <Box sx={{ pl: 3 }}>
+          {comment.attributes.child_comments.map((child_comment) => (
+            <Comment
+              comment={child_comment.data}
+              setComments={setComments}
+              key={child_comment.id}
+            />
+          ))}
+        </Box>
+      )}
     </>
   );
 }
