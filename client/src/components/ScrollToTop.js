da468c284eaa4e3a424821994affadf9a53f325c
Tao Liu <taoliudesign@gmail.com>
2023-07-21 16:43:49 -0700

    fix scrollToTop colors

--- client/src/components/ScrollToTop.js
+++ client/src/components/ScrollToTop.js
@@ -1,15 +1,15 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import CssBaseline from '@mui/material/CssBaseline';
-import useScrollTrigger from '@mui/material/useScrollTrigger';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Fab from '@mui/material/Fab';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import Fade from '@mui/material/Fade';
+import * as React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import CssBaseline from "@mui/material/CssBaseline";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Fab from "@mui/material/Fab";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import Fade from "@mui/material/Fade";
 
 function ScrollTop(props) {
   const { children, window } = props;
@@ -24,12 +24,12 @@ function ScrollTop(props) {
 
   const handleClick = (event) => {
     const anchor = (event.target.ownerDocument || document).querySelector(
-      '#back-to-top-anchor',
+      "#back-to-top-anchor"
     );
 
     if (anchor) {
       anchor.scrollIntoView({
-        block: 'center',
+        block: "center",
       });
     }
   };
@@ -39,7 +39,7 @@ function ScrollTop(props) {
       <Box
         onClick={handleClick}
         role="presentation"
-        sx={{ position: 'fixed', bottom: 16, right: 16 }}
+        sx={{ position: "fixed", bottom: 16, right: 16 }}
       >
         {children}
       </Box>
@@ -50,8 +50,18 @@ function ScrollTop(props) {
 export default function BackToTop(props) {
   return (
     <React.Fragment>
-          <ScrollTop {...props}>
-        <Fab size="small" aria-label="scroll back to top">
+      <ScrollTop {...props}>
+        <Fab
+          size="small"
+          aria-label="scroll back to top"
+          sx={{
+            backgroundColor: "gray",
+            color: "white",
+            "&:hover": {
+              backgroundColor: "gray",
+            },
+          }}
+        >
           <KeyboardArrowUpIcon />
         </Fab>
       </ScrollTop>
