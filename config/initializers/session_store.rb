417e1727f07a6e15135e9acc6e8237c19a0ae140
taoliu12 <taoliudesign@gmail.com>
2023-11-05 21:44:17 -0800

    fix cookies & add routes

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 Rails.application.config.session_store :cookie_store, 
   key: '_your_app_session',
   secure: Rails.env.production?, # 'true' for HTTPS
-  same_site: :none, # Adjust as per your needs (strict, lax, none)
+  same_site: :lax, # Adjust as per your needs (strict, lax, none)
   domain: :all # Allows cookies to be sent to different subdomains
--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,37 @@
 Rails.application.routes.draw do
-  get 'posts', to:'posts#index'
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/destroy'
-  get 'posts/show'
-  get 'posts/update'
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  
-  get "/userInSession", to:"sessions#get_logged_in_user"
+  scope '/api' do
+    get 'posts', to:'posts#index'
+    get 'posts/new'
+    get 'posts/create'
+    get 'posts/destroy'
+    get 'posts/show'
+    get 'posts/update'
+    get 'sessions/new'
+    get 'sessions/create'
+    get 'sessions/destroy'
+    
+    get "/userInSession", to:"sessions#get_logged_in_user"
 
-  root to: 'posts#index'
+    root to: 'posts#index'
 
-  resources :posts, except: [:index] do
-    resources :votes, only: [:create]
-    resources :comments 
+    resources :posts, except: [:index] do
+      resources :votes, only: [:create]
+      resources :comments 
+    end
+
+    resources :users, only: [:show] do
+      resources :posts, only: [:index]
+    end
+
+    resources :sessions, only: [:destroy]
+
+    get '/signup', to: 'users#new'
+    post '/signup', to: 'users#create'
+
+    get '/auth/:provider/callback', to: 'sessions#create_from_omniauth'
+
+    get '/login', to: 'sessions#new'
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
   end
-
-  resources :users, only: [:show] do
-    resources :posts, only: [:index]
-  end
-
-  resources :sessions, only: [:destroy]
-
-  get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
-
-  get '/auth/:provider/callback', to: 'sessions#create_from_omniauth'
-
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
 end
