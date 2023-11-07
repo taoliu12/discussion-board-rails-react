4190e4b1f78fac66688fc25f29598e214ba8d8be
taoliu12 <taoliudesign@gmail.com>
2023-11-07 10:20:18 -0800

    add host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,9 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Allow the specified host to access the application
+  config.hosts << "talkyapi.taoliudev.net"
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
